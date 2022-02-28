dc86944b4917467b9f92538fd88a2039a77143cf
Roberto Hernández Tamayo <roberto.htamayo@gmail.com>
2022-02-28 13:56:23 -0600

    first commit

---
+++ app/src/main/java/com/devexperto/kda/myplayerkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devexperto.kda.myplayerkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
