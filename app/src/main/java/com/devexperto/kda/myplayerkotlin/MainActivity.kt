4449500eab1ee248f6a3e30d7b32395da6f9d3b6
Roberto Hernández Tamayo <roberto.htamayo@gmail.com>
2022-03-30 18:30:19 -0600

    Agregando progressView y courutinas para consulta de datos

--- app/src/main/java/com/devexperto/kda/myplayerkotlin/MainActivity.kt
+++ app/src/main/java/com/devexperto/kda/myplayerkotlin/MainActivity.kt
@@ -4,18 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ProgressBar
+import androidx.lifecycle.lifecycleScope
 import com.devexperto.kda.myplayerkotlin.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-    private val adapter by lazy { MediaAdapter(MediaProvider.getItems()) { toast(it) } }
+    private val adapter = MediaAdapter { toast(it) }
+    private lateinit var progress: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        progress = binding.progress
 
         binding.recycler.adapter = adapter
+        updateItems()
+    }
+
+    private fun updateItems(filter: Int = R.id.filter_all) {
+        lifecycleScope.launch {
+            progress.visibility = View.VISIBLE
+            adapter.items = withContext(Dispatchers.IO) { getFilterItems(filter) }
+            progress.visibility = View.GONE
+        }
+    }
+
+    private fun getFilterItems(filter: Int) : List<MediaItem> {
+        return MediaProvider.getItems().let { media ->
+            when (filter) {
+                R.id.filter_all -> media.filter { it.type == Type.PHOTO }
+                R.id.filter_photos -> media.filter { it.type == Type.VIDEO }
+                R.id.filter_videos -> MediaProvider.getItems()
+                else -> emptyList()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,14 +52,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        adapter.items = MediaProvider.getItems().let { media ->
-            when (item.itemId) {
-                R.id.filter_all -> media.filter { it.type == Type.PHOTO }
-                R.id.filter_photos -> media.filter { it.type == Type.VIDEO }
-                R.id.filter_videos -> MediaProvider.getItems()
-                else -> emptyList()
-            }
-        }
+        updateItems(item.itemId)
         return super.onOptionsItemSelected(item)
     }
 }
